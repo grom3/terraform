959340fe3e6d9668b578cd09b66530b5558c7149
jerome.vanandruel@gmail.com <jerome.vanandruel@gmail.com>
2020-03-06 09:48:59 +0000

    test

--- instance_aws.tf
+++ instance_aws.tf
@@ -1,15 +1,16 @@
 resource "aws_instance" "instancelinux" {
-  instance_type          = "t2.micro"
-  ami                    = "ami-0ff760d16d9497662"
-  key_name               = "formation-hpc"
-  subnet_id              = "subnet-195cd142"
-  vpc_security_group_ids = ["sg-06678f94fa9ec164f"]
-
+  instance_type = "t2.micro"
+  ami = "ami-0ff760d16d9497662"
+  key_name = "formation-hpc"
+  subnet_id = "subnet-195cd142"
+  vpc_security_group_ids = [
+    "sg-06678f94fa9ec164f",
+  ]
   tags = "{
-          Name = "codeherent
-          }"
+          Name = "
+}
 
-  lifecycle {
-    ignore_changes = ["user_data", "ami", "root_block_device"]
+user_data ", " ami ", " root_block_device "]
   }
 }
+" {}
