9a3adbe7aa73e801eeba989e9bc22f6e86111698
grom3 <61865152+grom3@users.noreply.github.com>
2020-03-06 11:15:23 +0100

    Update instance_aws.tf

--- instance_aws.tf
+++ instance_aws.tf
@@ -3,16 +3,11 @@ resource "aws_instance" "instancelinux" {
   ami = "ami-0ff760d16d9497662"
   key_name = "formation-hpc"
   subnet_id = "subnet-195cd142"
-  vpc_security_group_ids = [
-    "sg-06678f94fa9ec164f",
-  ]
-  tags = "{ Name = "Codeherent"}"
-}
-
-user_data ", " ami ", " root_block_device "]
+  vpc_security_group_ids = ["sg-06678f94fa9ec164f"]
+  tags = { 
+    Name = "Codeherent"
   }
 }
-" {}
 
 provider "aws" {
   region = eu-west-1
