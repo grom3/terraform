d8bff6b4235dc00afa056e15af8ff23581a10429
jerome.vanandruel@gmail.com <jerome.vanandruel@gmail.com>
2020-03-06 10:49:09 +0000

    test

--- instance_aws.tf
+++ instance_aws.tf
@@ -1,16 +1,18 @@
 resource "aws_instance" "instancelinux" {
   instance_type = "t2.micro"
-  ami = "ami-0ff760d16d9497662"
-  key_name = "formation-hpc"
-  subnet_id = "subnet-195cd142"
-  vpc_security_group_ids = ["sg-06678f94fa9ec164f"]
-  tags = { 
+  ami           = "ami-0ff760d16d9497662"
+  key_name      = "formation-hpc"
+  subnet_id     = "subnet-195cd142"
+  vpc_security_group_ids = [
+    "sg-06678f94fa9ec164f",
+  ]
+  tags = {
     Name = "Codeherent"
   }
 }
 
 provider "aws" {
-  region = eu-west-1
-  access_key = 
-  secret_key = 
+  region     = eu-west-1
+  access_key = etea
+  secret_key = rezaraer
 }
